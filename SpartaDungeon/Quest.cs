9f8a3355aea19fdd8b87d9448478ac42cdd6f393
ohdodog <97august4@naver.com>
2024-05-01 14:47:16 +0900

    퀘스트 기본틀 작성

--- SpartaDungeon/Quest.cs
+++ SpartaDungeon/Quest.cs
@@ -10,25 +10,73 @@ namespace SpartaDungeon
     {
         public string Name { get; }
         public string Desc { get; }
-        public int Require {  get; }
-        public string RewardItem { get; }
+        public string Require {  get; }
+        //public string RewardItem { get; }
         public int RewardGold { get; }
-        
 
-        public Quest(string name, string desc, int require, string rewardItem, int rewardGold)
+        public bool IsAccept { get; private set; }
+        public bool IsClear { get; private set; }
+
+
+        public Quest(string name, string desc, string require, /*string rewardItem,*/ int rewardGold, bool isAccept = false ,bool isClear = false)
         {
             Name = name;
             Desc = desc;
             Require = require;
-            RewardItem = rewardItem;
+            //RewardItem = rewardItem;
             RewardGold = rewardGold;
+            IsAccept = isAccept;
+            IsClear = isClear;
         }
 
 
-        //퀘스트 목록 보여주기
+        public void PrintQuestList(int idx)  //퀘스트 목록 보여주기
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write($"{idx}. ");
+            Console.ResetColor();
 
-        //퀘스트 수락하기
+            if(IsAccept)
+            {
+                Console.Write(Name);
+                Console.WriteLine(" - 수령 완료");
+            }
+            else
+            {
+                Console.WriteLine(Name);
+            }
 
-        //퀘스트 완료하기,보상받기
+        }
+
+        public void PrintQuestInfo()  // 퀘스트 정보 보여주기
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine("");
+
+            Console.WriteLine(Desc);
+        }
+
+        public void PrintMyQuestList(int idx)  // 수령한 퀘스트 목록 보여주기
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write($"{idx}. ");
+            Console.WriteLine(Name);
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine(Desc);
+            Console.WriteLine("");
+
+        }
+
+        public void AcceptQuest()  //퀘스트 수락처리
+        {
+            IsAccept = true;
+            IsClear =false;
+        }
+
+        public void ClearQuest()  //퀘스트 완료처리
+        {
+            IsClear = true;
+        }
     }
 }
