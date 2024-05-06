2e8b64cc9be92b99fa705537dbe4cbe90e371fc1
MilkyQuartz <milkyquartz.2001@gmail.com>
2024-05-06 23:33:49 +0900

    거슬리는 Main UI 수정

--- SpartaDungeon/Program.cs
+++ SpartaDungeon/Program.cs
@@ -80,10 +80,10 @@ namespace SpartaDungeon
             Console.Clear();
 
             // 1. 선택 멘트를 줌
-            Console.WriteLine("■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+            Console.WriteLine("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
             Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
             Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
-            Console.WriteLine("■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+            Console.WriteLine("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
             Console.WriteLine("");
 
             Console.WriteLine("1. 상태보기");
